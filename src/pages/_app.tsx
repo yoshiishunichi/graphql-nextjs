9a1f0023af5038872ed2f6240b1cd91735220d70
吉井駿一 <syun_syun01@icloud.com>
2022-09-10 17:06:26 +0900

    :sparkles: Query追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "styles/globals.scss";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
 import type { AppProps } from "next/app";
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "http://localhost:4000/graphql",
+});
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
+);
 
 export default MyApp;
